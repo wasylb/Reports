69f0b00616dd19c753e778d83b7b74fb61bbc061
Bartosz Wasilew <barofficia@vp.pl>
2020-02-03 16:46:59 +0100

    Project structure setup | json-server mocks

---
+++ reports-app/src/app/general/general.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReportsComponent } from './components/reports/reports.component';
+
+
+
+@NgModule({
+  declarations: [ReportsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GeneralModule { }
