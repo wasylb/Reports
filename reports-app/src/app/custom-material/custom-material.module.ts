cd6cf550b2dab06c4ebbaaf1f1c3681038479cf4
Bartosz Wasilew <barofficia@vp.pl>
2020-02-03 17:14:09 +0100

    Custom material module implementation | rxjs installation

--- reports-app/src/app/custom-material/custom-material.module.ts
+++ reports-app/src/app/custom-material/custom-material.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatButtonModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatTableModule,
+    MatDatepickerModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatSelectModule,
+    MatTableModule,
+    MatDatepickerModule
   ]
 })
 export class CustomMaterialModule { }
--- reports-app/src/app/general/general.module.ts
+++ reports-app/src/app/general/general.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReportsComponent } from './components/reports/reports.component';
+import { CustomMaterialModule } from '../custom-material/custom-material.module';
 
 
 
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CustomMaterialModule
+  ],
+  exports: [
+    ReportsComponent
   ]
 })
 export class GeneralModule { }
